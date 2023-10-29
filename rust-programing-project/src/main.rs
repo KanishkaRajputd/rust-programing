a07d0a4242606384533e8061043faaa9e4bebc89
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-10-29 13:49:04 +0530

    introducing mut

--- rust-programing-project/src/main.rs
+++ rust-programing-project/src/main.rs
@@ -3,13 +3,21 @@ fn main() {
 
     //variables 
 
-    let x = 2;
-    println!("the value of x is {x}")
-    // the value of x is 2
+    // let x = 2;
+    // println!("the value of x is {x}")
+    // // the value of x is 2
 
-    re-assignning the value
+    // re-assignning the value
+    // x = 5;
+    // println!("the value of x is {x}")
+    // // error -  first assignment to `x` help: consider making this binding mutable: `mut x`
+
+    let mut x = 3;
     x = 5;
+    
     println!("the value of x is {x}")
-    // error -  first assignment to `x` help: consider making this binding mutable: `mut x`
+    //help: maybe it is overwritten before being read?
+    //note: `#[warn(unused_assignments)]` on by default
+    // the value of x is 5
 
 }
