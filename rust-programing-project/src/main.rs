b5c3744da5df6bc7b8bf9bc02c01aa67dc36dce8
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-10-29 13:48:09 +0530

    reassigning the value and print the value

--- rust-programing-project/src/main.rs
+++ rust-programing-project/src/main.rs
@@ -7,4 +7,9 @@ fn main() {
     println!("the value of x is {x}")
     // the value of x is 2
 
+    re-assignning the value
+    x = 5;
+    println!("the value of x is {x}")
+    // error -  first assignment to `x` help: consider making this binding mutable: `mut x`
+
 }
